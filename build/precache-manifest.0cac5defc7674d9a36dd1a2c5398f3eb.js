123305288fea5ece54a0b687851eee137c403bbc
Perry Cheung <ahstop1112@hotmail.com>
2019-10-22 02:08:08 +0800

    add sass-loader

---
+++ build/precache-manifest.0cac5defc7674d9a36dd1a2c5398f3eb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38ad6544f555565b7e0597f830f5262d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d864a1bd5a82dbc6ae33",
+    "url": "/static/css/3.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "a404f5669870ca8731ce",
+    "url": "/static/css/main.1c127a40.chunk.css"
+  },
+  {
+    "revision": "d864a1bd5a82dbc6ae33",
+    "url": "/static/js/3.7238a777.chunk.js"
+  },
+  {
+    "revision": "796e79176c9dc926b76b",
+    "url": "/static/js/4.49560cb8.chunk.js"
+  },
+  {
+    "revision": "ad9aefaf11db4269b0ee",
+    "url": "/static/js/Home.fdbc7fba.chunk.js"
+  },
+  {
+    "revision": "a404f5669870ca8731ce",
+    "url": "/static/js/main.382a9e02.chunk.js"
+  },
+  {
+    "revision": "d40c4c5715661e4827cf",
+    "url": "/static/js/runtime~main.e53e71d4.js"
+  }
+]);
